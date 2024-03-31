3ac8a8bdb633e1b4d9b17305e0235c4b427ace5d
The Amaral <82917923+AmaralRage@users.noreply.github.com>
2024-03-31 13:57:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
- function toggleColor() {
-    var card = document.getElementById('card');
-    card.classList.toggle('dark-mode');
- }
+// function toggleColor() {
+  //  var card = document.getElementById('card');
+    // card.classList.toggle('dark-mode');
+ // }
 
+function toggleColor() {
+    var card = document.getElementById('card');
+    var icon = document.getElementById("icon");
+    card.classList.toggle('dark-mode');
+    if (card.classList.contains('dark-mode')) {
+        // Se o modo escuro estiver ativado, mude o ícone para uma lua
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+    } else {
+        // Caso contrário, mantenha o ícone do sol
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+    }
+}
