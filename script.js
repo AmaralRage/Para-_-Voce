089fc9e91131445ee41d14ea0da4e036ee9bb2c5
The Amaral <82917923+AmaralRage@users.noreply.github.com>
2024-03-31 13:48:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-// function toggleColor() {
-//    var card = document.getElementById('card');
-//    card.classList.toggle('dark-mode');
-// }
+ function toggleColor() {
+    var card = document.getElementById('card');
+    card.classList.toggle('dark-mode');
+ }
 
-
-function toggleColor() {
-    var icon = document.getElementById("icon");
-    if (icon.classList.contains("fa-sun")) {
-        icon.classList.remove("fa-sun");
-        icon.classList.add("fa-moon");
-    } else {
-        icon.classList.remove("fa-moon");
-        icon.classList.add("fa-sun");
-    }
-}
